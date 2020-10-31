4ec5a57b64ac3844032ce788e38c4505d80f821f
Rakesh Akhileswaran <Rakesh.Akhileswaran@in.ey.com>
2020-10-31 11:48:59 +0530

    initial structure commit

---
+++ HoneyWell.Flight.PriorityService.Api/Controllers/PriorityController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace HoneyWell.Flight.PriorityService.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PriorityController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetPriority(int flighId, string flightTypes, string passengerclass)
+        {
+            return Ok();
+        }
+    }
+}
