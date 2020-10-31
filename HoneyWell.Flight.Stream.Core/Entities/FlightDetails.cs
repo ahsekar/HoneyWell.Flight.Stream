6b018d0f985497aa9a0287443432026e343639c6
Chandan Kumar <Chandan_Kumar22@DellTeam.com>
2020-10-31 12:48:22 +0530

    Added DataAnnotations for Entities

--- HoneyWell.Flight.Stream.Core/Entities/FlightDetails.cs
+++ HoneyWell.Flight.Stream.Core/Entities/FlightDetails.cs
@@ -1,12 +1,15 @@
 ﻿using HoneyWell.Flight.Stream.Core.Enums;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace HoneyWell.Flight.Stream.Core.Entities
 {
 	public class FlightDetails
 	{
+		[Key]
+		public int Id { get; set; }
 		public Flight Flight { get; set; }
 		public double DataSize { get; set; }
 		public DataTransferStatus DataTransferStatus { get; set; }
