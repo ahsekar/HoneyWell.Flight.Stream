3fe9a2c994e6623e77bba1bd63bcea32d4fceaca
Tamal Sain <tamal.sain@gmail.com>
2020-10-31 12:56:04 +0530

    Repository Changes

---
+++ UploadSvc/RepositoryOperation/IRepositoryOperation.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UploadSvc.RepositoryOperation
+{
+    public interface IRepositoryOperation
+    {
+
+    }
+}
