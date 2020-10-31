176483863e62fc01c576141f337c56ceb6f12056
Rakesh Akhileswaran <Rakesh.Akhileswaran@in.ey.com>
2020-10-31 12:07:46 +0530

    Added Enums

--- HoneyWell.Flight.PriorityService.Api/Models/RuleItems.cs
+++ HoneyWell.Flight.PriorityService.Api/Models/RuleItems.cs
@@ -1,10 +1,13 @@
-﻿namespace HoneyWell.Flight.PriorityService.Api.Models
+﻿using HoneyWell.Flight.PriorityService.Api.Constants;
+
+namespace HoneyWell.Flight.PriorityService.Api.Models
 {
     public class RuleItems
     {
         public int priority { get; set; }
         public int FlightId { get; set; }
-        public int FlightTypes { get; set; }
-        public int PassengerClass { get; set; }
+        public FlightTypes FlightTypes { get; set; }
+        public PassengerClass PassengerClass { get; set; }
+        //More Rules can be added
     }
 }
