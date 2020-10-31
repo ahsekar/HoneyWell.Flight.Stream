f70fecdb32c1c6da7402644b053256be7a222bc9
himanchalsingh93 <himanchalsingh93@gmail.com>
2020-10-31 11:41:29 +0530

    added models

---
+++ UploadSvc/Models/MediaFile.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UploadSvc.Models
+{
+    public class MediaFile
+    {
+        public int MediaFileId { get; set; }
+        public string Type { get; set; }
+        public long Size { get; set; }
+        public string Name { get; set; }
+        public string Genre { get; set; }
+    }
+}
