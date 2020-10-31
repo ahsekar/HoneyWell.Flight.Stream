d9274936311ef9fbcdb8d88961ff1e7ad7a8a212
Rakesh Akhileswaran <Rakesh.Akhileswaran@in.ey.com>
2020-10-31 12:02:29 +0530

    Added business class and models

---
+++ HoneyWell.Flight.PriorityService.Api/Models/RuleItems.cs
@@ -0,0 +1,10 @@
+﻿namespace HoneyWell.Flight.PriorityService.Api.Models
+{
+    public class RuleItems
+    {
+        public int priority { get; set; }
+        public int FlightId { get; set; }
+        public int FlightTypes { get; set; }
+        public int PassengerClass { get; set; }
+    }
+}
